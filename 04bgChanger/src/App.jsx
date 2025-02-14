38d3907973cd697b971de571174861311751aac2
somilnegi <somilnegi12345@gmail.com>
2025-02-14 23:16:14 +0530

    Adding bgChanger project folder

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+
+function App() {
+
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className="w-full h-screen" style={{backgroundColor: color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-20 inset-x-0" >
+        <div className="flex flex-wrap justify-between w-300 gap-3 shadow-xl bg-white px-3 py-2 rounded-2xl">
+          <button onClick={() => setColor("red")} className="outline-none px-4 py-2 rounded-2xl text-white shadow-lg" style={{backgroundColor: "red"}}>Red</button>
+          <button onClick={() => setColor("yellow")} className="outline-none px-4  rounded-2xl text-black shadow-lg" style={{backgroundColor: "yellow"}}>Yellow</button>
+          <button onClick={() => setColor("purple")} className="outline-none px-4 py-1 rounded-2xl text-white shadow-lg" style={{backgroundColor: "purple"}}>Purple</button>
+          <button onClick={() => setColor("lavender")} className="outline-none px-4 py-1 rounded-2xl text-black shadow-lg" style={{backgroundColor: "lavender"}}>Lavender</button>
+          <button onClick={() => setColor("pink")} className="outline-none px-4 py-1 rounded-2xl text-black shadow-lg" style={{backgroundColor: "pink"}}>Pink</button>
+          <button onClick={() => setColor("green")} className="outline-none px-4 py-1 rounded-2xl text-white shadow-lg" style={{backgroundColor: "green"}}>Green</button>
+          <button onClick={() => setColor("blue")} className="outline-none px-4 py-1 rounded-2xl text-white shadow-lg" style={{backgroundColor: "blue"}}>Blue</button>
+          <button onClick={() => setColor("brown")} className="outline-none px-4 py-1 rounded-2xl text-white shadow-lg" style={{backgroundColor: "brown"}}>Brown</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
